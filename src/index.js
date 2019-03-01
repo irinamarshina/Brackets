fa4341b82db7b5b5145a7e58ffd3e34a51bee133
irinamarshina <31935561+irinamarshina@users.noreply.github.com>
2019-03-01 10:43:40 +0300

    Cange in if

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-	//str=str.split(""); //split each character into an array
-	let arr=[]; //work array
-    //iterate over the charters and determine the truth
-	for (var i = 0; i <str.length; i++) {
-		for (var j = 0; j <bracketsConfig.length; j++) {
-			if(str[i]==bracketsConfig[j][1]&&arr[arr.length-1]==bracketsConfig[j][0]){
-				arr.pop();
-			}else if(str[i]==bracketsConfig[j][0]){
+	var arr=[];
+	for (var i = 0; i <str.length; i++) { //перебираем строку
+		for (var j = 0; j <bracketsConfig.length; j++) { 
+			if(str[i]==bracketsConfig[j][0]&&str[str.length-(i+1)]==bracketsConfig[j][1]){ //проверяем левые и правые скобки
 				arr.push(str[i]);
+			}else if(str[i]==bracketsConfig[j][1]){
+				arr.pop(str[i]);
 			}
 		}
 	}
-	//returns result to test
-	return arr.length==0?true:false;
+	return arr.length==0 && str.length % 2==0 ? true : false; //вывод результата
 }
